d3e0fedcda0686d2cda01daaf99538b2b1b32ac0
Michael Wanyoike <micwanyoike@gmail.com>
2021-03-13 13:38:44 +0300

    Implemented JSON handling and HTTP testing

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+  const json = await request.json()
+  const { firstName } = json
+  const data = {
+    message: `Hello ${firstName}!`,
+  }
+  return new Response(JSON.stringify(data), {
+    headers: { 'content-type': 'text/json' },
   })
 }
